45d4cd336124bd323fea5e389e47bf4839d986b9
Ryan Osterman <ryanosterman10@dhcp-v880-9-00790.1Xwireless.unc.edu>
2016-08-13 12:50:35 -0400

    minor updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 		squad : [
 			{
-				name : "harry",
+				name : "Harry",
 				HP: 300,
 				currentHP: 300,
 				attackPow: 25,
@@ -12,15 +12,15 @@ $(document).ready(function(){
 				counterPow: 25
 			},
 			{
-				name : "dumbledore",
+				name : "Dumbledore",
 				HP: 450,
-				currentHP: 400,
+				currentHP: 450,
 				attackPow: 10,
 				incAttackPow: 10,
 				counterPow: 10
 			},
 			{
-				name : "voldemort",
+				name : "Voldemort",
 				HP: 400,
 				currentHP: 400,
 				attackPow: 15,
@@ -28,7 +28,7 @@ $(document).ready(function(){
 				counterPow: 15
 			},
 			{
-				name : "snape",
+				name : "Snape",
 				HP: 350,
 				currentHP: 350,
 				attackPow: 20,
@@ -43,33 +43,64 @@ $(document).ready(function(){
 		reset : function(){
 
 
-		} //function close
+		}, //reset close
+
+		attack : function(){
 
 
 
+		}, // attack close
 
 
+		counterAttack : function(){
 
 
-
-
-
-
+		} // counterAttack close
 
 	}  //potterRPG close
 
-
-
-
-
-
-	$('img').click(function(){
-		// $(this).fadeOut('slow');
-		$(this).appendTo('#left-of-game');
+	$('button').on('click', function(){
+		potterRPG.attack(console.log("attack"));
+		potterRPG.counterAttack(console.log("counterAttack"));
 	});
 
 
 
 
+	$('.characters').on('click', function(){
+		$(this).appendTo('#left-of-game');
+		$('.characters').not(this).addClass('opponent');
+			// if($(this).is('#harry')){
+			// 	$('#left-of-game').append("Character: " + potterRPG.squad[0].name + "<br>" + "HP: " + potterRPG.squad[0].currentHP);
+			// }
+			// else if($(this).is('#dumbledore')){
+			// 	$('#left-of-game').append("Character: " + potterRPG.squad[1].name + "<br>" + "HP: " + potterRPG.squad[1].currentHP);
+			// }
+			// else if($(this).is('#voldemort')){
+			// 	$('#left-of-game').append("Character: " + potterRPG.squad[2].name + "<br>" + "HP: " + potterRPG.squad[2].currentHP);
+			// }
+			// else if($(this).is('#snape')){
+			// 	$('#left-of-game').append("Character: " + potterRPG.squad[3].name + "<br>" + "HP: " + potterRPG.squad[3].currentHP);
+			// }
+		if($(this).hasClass('opponent')){
+			$(this).appendTo('#right-of-game');
+			if($(this).is('#harry')){
+				$('#right-of-game').append("Character: " + potterRPG.squad[0].name + "<br>" + "HP: " + potterRPG.squad[0].currentHP);
+			}
+			else if($(this).is('#dumbledore')){
+				$('#right-of-game').append("Character: " + potterRPG.squad[1].name + "<br>" + "HP: " + potterRPG.squad[1].currentHP);
+			}
+			else if($(this).is('#voldemort')){
+				$('#right-of-game').append("Character: " + potterRPG.squad[2].name + "<br>" + "HP: " + potterRPG.squad[2].currentHP);
+			}
+			else if($(this).is('#snape')){
+				$('#right-of-game').append("Character: " + potterRPG.squad[3].name + "<br>" + "HP: " + potterRPG.squad[3].currentHP);
+			}
+		}
+	});
+
+
+
 
 });	 //document.ready close
+
